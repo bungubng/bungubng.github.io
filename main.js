190943934628c2ecae2d04215d401fd898fcc52b
Bryan Bungubung <bryanbungubung@uvic.ca>
2026-04-09 22:15:13 -0700

    learn uniforms

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const vertexShaderSource = `#version 300 es
 #pragma vscode_glsllint_stage: vert
 
+uniform float uPointSize;
+uniform vec2 uPosition;
+
 void main() 
 {
-    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-    gl_PointSize = 150.0;
+    gl_Position = vec4(uPosition, 0.0, 1.0);
+    gl_PointSize = uPointSize;
 }`;
 
 const fragmentShaderSource = `#version 300 es
@@ -12,11 +15,14 @@ const fragmentShaderSource = `#version 300 es
 
 precision mediump float;
 
+uniform int uIndex;
+uniform vec4 uColors[3];
+
 out vec4 frag_color;
 
 void main() 
 {
-    frag_color = vec4(0.0, 0.0, 0.0, 1.0);
+    frag_color = uColors[uIndex];
 }`;
 
 const canvas = document.querySelector('canvas');
@@ -41,5 +47,21 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
     console.log(gl.getShaderInfoLog(fragment_shader));
 }
 gl.useProgram(program);
+
+const uPositionLoc = gl.getUniformLocation(program, 'uPosition');
+gl.uniform2f(uPositionLoc, 0, -.7);
+
+const uPointSizeLoc = gl.getUniformLocation(program, 'uPointSize');
+gl.uniform1f(uPointSizeLoc, 100);
+
+const uIndexLoc = gl.getUniformLocation(program, 'uIndex');
+const uColorsLoc = gl.getUniformLocation(program, 'uColors');
+
+gl.uniform1i(uIndexLoc, 1);
+gl.uniform4fv(uColorsLoc, [
+    1,0,0,1,
+    0,.5,.5,1,
+    0,0,1,1,
+]);
 
 gl.drawArrays(gl.POINTS, 0, 1);
